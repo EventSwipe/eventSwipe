99b24d7bd28eb896ca11802507f9cdaecae2283e
Danny Viterbo <dannyviterbo@gmail.com>
2018-09-05 17:45:44 -0400

    (feat) added info to favorites list

--- react-client/src/components/FavoritesList.jsx
+++ react-client/src/components/FavoritesList.jsx
@@ -4,7 +4,16 @@ import Favorites from './Favorites.jsx';
 // just make a list of all the likes passed down by props
 const FavoritesList = ({ favorites }) => (
   <div>
-    {favorites.map(favorite => <Favorites favorite={favorite} key={favorite.id}/>)}
+    <h4> Faves </h4>
+    <ul>
+      {favorites.map(favorite => 
+        <li Favorites favorite={favorite} key={favorite.id}>
+        {favorite.name}, 
+        {favorite.start.local.substr(0, 10)}, {favorite.start.local.substr(11)},
+        <a href={favorite.url}>Get Tickets!!!</a>, 
+        </li> 
+      )}
+    </ul>
   </div>
 )
 
