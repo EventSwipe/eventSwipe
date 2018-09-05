f8d890ab756590e336ea40ba654e617980c5b68c
Alon Bibring <alon.bibring@gmail.com>
2018-09-05 19:55:11 -0400

    have api being called on componentDidMount of Events and it works

--- react-client/src/components/Events.jsx
+++ react-client/src/components/Events.jsx
@@ -11,11 +11,13 @@ class Events extends React.Component {
     this.dislikeEvent = this.dislikeEvent.bind(this);
     this.addEventToFaves = this.addEventToFaves.bind(this);
     this.requestEvents = this.requestEvents.bind(this);
+    this.getIt = this.getIt.bind(this);
   }
   componentDidMount() {
-    this.setState({ events: this.props.events })
+    this.getIt();
   }
   addEventToFaves() {
+    console.log('123');
     if (this.state.count >= this.state.events.length-1) {
       this.setState({ count: 0 })
     } else {
@@ -23,11 +25,22 @@ class Events extends React.Component {
     }
   }
   dislikeEvent() {
+    console.log('456');
     if (this.state.count >= this.state.events.length-1) {
       this.setState({ count: 0 })
     } else {
       this.setState({ count: this.state.count+1 });
     }
+  }
+
+  getIt() {
+    axios.get('https://www.eventbriteapi.com/v3/events/search/?token=E5PTH3KVZH4MFUMMULAE&q=club&location.address=newyorkcity')
+    .then(({data}) => {
+      console.log('1221212212', data.events);
+      this.setState({ events: data.events });
+      console.log(this.state.events)
+    })
+    .catch((err) => console.log('front end error', err))
   }
 
 
@@ -38,7 +51,7 @@ class Events extends React.Component {
   render() {
     return (
       <div>
-        {console.log(`22222: ${JSON.stringify(this.state.events)}`)}  
+        {/* {console.log(`22222: ${JSON.stringify(this.state.events)}`)}   */}
         <Event event={this.state.events[this.state.count]} like={this.addEventToFaves} dislike={this.dislikeEvent}/> 
 
       </div>
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -11,31 +11,24 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      events: dummyData.events,
+      events: [],
       favorites: [],
       showFaves: false,
     }
     this.postEvents = this.postEvents.bind(this);
     this.showFavorites = this.showFavorites.bind(this);
+    // this.getIt = this.getIt.bind(this);
   }
 
   // //uses getEvents helper method on did mount to load all events
-  componentDidMount() {
-  //  this.getEvents();
-  console.log(`dummyData: ${JSON.stringify(this.state.favorites)}`)
-  }
 
   //sends get request to server to get the events from api
   postEvents() {
     console.log("hitting the getMOVIES in client");
     axios
       .post("/events")
-      .then(res => {
-        // console.log('RES.DATA in axios.postEvents', res.data)  
-        this.setState({ events: res.data })
-        // console.log('this is the STATE', this.state.events)
-      })
-      .catch(err => console.error(`err in getEvents in index.jsx: ${err}`));
+      .then(res => this.setState({ events: res.data }))
+      .catch(err => console.error(`err in postEvents in index.jsx: ${err}`));
   }
 
   showFavorites() {
@@ -44,7 +37,7 @@ class App extends React.Component {
 
   //renders navbar, even & likes
   render () {
-    const showFavesOrEvents = this.state.showFaves ? <FavoritesList favorites={this.state.favorites}/>: <Events events={this.state.events}/>;
+    const showFavesOrEvents = this.state.showFaves ? <FavoritesList favorites={this.state.favorites}/>: <Events />;
     return (
     <div>
       <NavBar/>
