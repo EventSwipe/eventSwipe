d670fe94f57ebb349e9bcf8f006c829022c9a3de
Paul Chung <pisc9211@gmail.com>
2018-09-06 17:23:02 -0400

    added the click and drag functionality

--- react-client/src/components/Event.jsx
+++ react-client/src/components/Event.jsx
@@ -6,13 +6,13 @@ const Event = ({ event, like, dislike }) => {
     <div>
       {event ? (
       <div>
-        <div className="column">
+        <div className="column" onDragOver={(e) => e.preventDefault()} onDrop={dislike}>
           <button className="float-right" onClick={dislike}>
             <span><img className="like-button float-right" style={{height: 100, width: 100}} src="https://pbs.twimg.com/profile_images/534074996562227200/OR7cp94I_400x400.png" alt="Like" /></span>
           </button>
         </div>
         <div className="column-center">
-          <img className="event-img" style={{width:410, height:300}} src={event.logo ? event.logo.url : 'http://www.jamiariyadhuljannah.com/wp-content/uploads/2014/02/500x300.png'} alt="Card image cap"/>
+          <img className="event-img" draggable="true" style={{width:410, height:300}} src={event.logo ? event.logo.url : 'http://www.jamiariyadhuljannah.com/wp-content/uploads/2014/02/500x300.png'} alt="Card image cap"/>
           <div className="card" >
             <div className="card-body">
               <h5 className="card-title">{event.name.text}</h5>
@@ -27,7 +27,7 @@ const Event = ({ event, like, dislike }) => {
             </div>
           </div>
         </div>
-        <div className="column">
+        <div className="column" onDragOver={(e) => e.preventDefault()} onDrop={like}>
           <button onClick={like}>
             <span><img className="like-button" style={{height: 100, width: 100}} src="http://web.arjentienkamp.com/codepen/tinder/heart.png" alt="Like" /></span>
           </button>
