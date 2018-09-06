179d0de5ab7787482828a34b5da163b882fa399a
Justin Chung <bearlyalive1337@gmail.com>
2018-09-06 18:55:54 -0400

    Fixed port issues

--- server/index.js
+++ server/index.js
@@ -89,6 +89,6 @@ app.post('/login', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-  console.log('listening on port 3000!');
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
