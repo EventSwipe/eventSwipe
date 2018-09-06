f1bc9d8b8f56fc12f3b6af02096daccd26ade1db
Alon Bibring <alon.bibring@gmail.com>
2018-09-06 17:31:24 -0400

    working on deploying with now

--- react-client/src/components/Event.jsx
+++ react-client/src/components/Event.jsx
@@ -5,36 +5,36 @@ const Event = ({ event, like, dislike }) => {
   return (
     <div>
       {event ? (
-      <div>
-        <div className="column">
-          <button className="float-right" onClick={dislike}>
-            <span><img className="like-button float-right" style={{height: 100, width: 100}} src="https://pbs.twimg.com/profile_images/534074996562227200/OR7cp94I_400x400.png" alt="Like" /></span>
-          </button>
-        </div>
-        <div className="column-center">
-          <img className="event-img" style={{width:410, height:300}} src={event.logo ? event.logo.url : 'http://www.jamiariyadhuljannah.com/wp-content/uploads/2014/02/500x300.png'} alt="Card image cap"/>
-          <div className="card" >
-            <div className="card-body">
-              <h5 className="card-title">{event.name.text}</h5>
-              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p className=" event-description card-text">{event.description.text}</p>
-              <p className="card-text"><span className="Loc">Venue Address: </span>
-                <span>{event.address ? event.address : 'TBD'}</span></p>
-              <p className="card-text"><span className="date">Date: </span>
-                <span>{event.start.local.substr(0, 10)}</span></p>
-              <p className="card-text"><span className="link">Link: </span>
-                <a href={event.url}>{event.url}</a></p>
+        <div>
+          <div className="column">
+            <button className="float-right" onClick={dislike}>
+              <span><img className="like-button float-right" style={{height: 100, width: 100}} src="https://pbs.twimg.com/profile_images/534074996562227200/OR7cp94I_400x400.png" alt="Like" /></span>
+            </button>
+          </div>
+          <div className="column-center">
+            <img className="event-img" style={{width:410, height:300}} src={event.logo ? event.logo.url : 'http://www.jamiariyadhuljannah.com/wp-content/uploads/2014/02/500x300.png'} alt="Card image cap"/>
+            <div className="card" >
+              <div className="card-body">
+                <h5 className="card-title">{event.name.text}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p className=" event-description card-text">{event.description.text}</p>
+                <p className="card-text"><span className="Loc">Venue Address: </span>
+                  <span>{event.address ? event.address : 'TBD'}</span></p>
+                <p className="card-text"><span className="date">Date: </span>
+                  <span>{event.start.local.substr(0, 10)}</span></p>
+                <p className="card-text"><span className="link">Link: </span>
+                  <a href={event.url}>{event.url}</a></p>
+              </div>
             </div>
           </div>
+          <div className="column">
+            <button onClick={like}>
+              <span><img className="like-button" style={{height: 100, width: 100}} src="http://web.arjentienkamp.com/codepen/tinder/heart.png" alt="Like" /></span>
+            </button>
+          </div>
         </div>
-        <div className="column">
-          <button onClick={like}>
-            <span><img className="like-button" style={{height: 100, width: 100}} src="http://web.arjentienkamp.com/codepen/tinder/heart.png" alt="Like" /></span>
-          </button>
-        </div>
-      </div>
       ) : (
-        'Loading...'
+       <img src="https://media1.tenor.com/images/38153b27905ce716003dfdecc4989bb0/tenor.gif?itemid=9255733"></img>
       )}
     </div>
   );
