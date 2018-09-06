4c0b7aa747e62033341cf3a3097ece3943dda247
Alon Bibring <alon.bibring@gmail.com>
2018-09-06 15:10:49 -0400

    merging code with dev

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -101,14 +101,14 @@ const addUser = (username, password) => {
 };
 
 // Testing for database functions:
-let q = {
-  name: 'ROOFTOP PARTY SATURDAY NIGHT | HUDSON TERRACE',
-  description: "For BOTTLE SERVICE please email us at BOOKING (Add details) \r\n \r\nROOFTOP PARTY SATURDAY NIGHT   \r\nat\r\nHUDSON TERRACE ROOFTOP    \r\nNEW YORK CITY NIGHTCLUB \r\n621 West 46th Street \r\n \r\nMusic by :\r\nDJ DUBBS & GUEST\r\n \r\nDoors Open at 11pm\r\n \r\nTABLE RESERVATION FOR BOTTLE SERVICE, B'DAY PARTY OR ANY EVENT Please  send us an email to BOOKING\r\n \r\nGirls free till 12am $20 after  & Gents $20 -30 ( please arrive early to avoid long lines ) \r\n \r\n 21 and over with proper ID /FINAL ENTRENCE IS UPTO THE DOORMAN Discretion\r\n \r\n Must show tickets or SAY ICLUBNYC LIST  AT THE DOOR TO GET RIGHT IN \r\n \r\nSTRICT DRESS CODE POLICY: -Gentlemen: Shoes, Button down shirts, and jeans are acceptable. No \r\nbaggy attire, Sneakers, Boots, or Hats are allowed. -Ladies: Heels & classy look Please\r\n \r\n \r\nCONTACT US\r\n www.iclubnyc.com\r\n \r\n \r\n\r\n\r\nCLICK ON IMAGE \r\n\r\n\r\n\r\n",
-  id: '17434703664',
-  url: 'https://www.eventbrite.com/e/rooftop-party-saturday-night-hudson-terrace-tickets-17434703668?aff=ebapi',
-  date: '2018-09-08T23:00:00',
-  free: true
-};
+// let q = {
+//   name: 'ROOFTOP PARTY SATURDAY NIGHT | HUDSON TERRACE',
+//   description: "For BOTTLE SERVICE please email us at BOOKING (Add details) \r\n \r\nROOFTOP PARTY SATURDAY NIGHT   \r\nat\r\nHUDSON TERRACE ROOFTOP    \r\nNEW YORK CITY NIGHTCLUB \r\n621 West 46th Street \r\n \r\nMusic by :\r\nDJ DUBBS & GUEST\r\n \r\nDoors Open at 11pm\r\n \r\nTABLE RESERVATION FOR BOTTLE SERVICE, B'DAY PARTY OR ANY EVENT Please  send us an email to BOOKING\r\n \r\nGirls free till 12am $20 after  & Gents $20 -30 ( please arrive early to avoid long lines ) \r\n \r\n 21 and over with proper ID /FINAL ENTRENCE IS UPTO THE DOORMAN Discretion\r\n \r\n Must show tickets or SAY ICLUBNYC LIST  AT THE DOOR TO GET RIGHT IN \r\n \r\nSTRICT DRESS CODE POLICY: -Gentlemen: Shoes, Button down shirts, and jeans are acceptable. No \r\nbaggy attire, Sneakers, Boots, or Hats are allowed. -Ladies: Heels & classy look Please\r\n \r\n \r\nCONTACT US\r\n www.iclubnyc.com\r\n \r\n \r\n\r\n\r\nCLICK ON IMAGE \r\n\r\n\r\n\r\n",
+//   id: '17434703664',
+//   url: 'https://www.eventbrite.com/e/rooftop-party-saturday-night-hudson-terrace-tickets-17434703668?aff=ebapi',
+//   date: '2018-09-08T23:00:00',
+//   free: true
+// };
 
 // addFavorite(q, (err) => {
 //   // console.log(`q: ${q}`);
