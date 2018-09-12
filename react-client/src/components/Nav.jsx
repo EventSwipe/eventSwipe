8ff213ebc71b0f26c6917f0859ad0964c45ce48a
Alon Bibring <alon.bibring@gmail.com>
2018-09-12 17:01:19 -0400

    (fix) (refactor) cleaned up navbar. Signout button functionality needs to be implemented with the signout button in the navbar

--- react-client/src/components/Nav.jsx
+++ react-client/src/components/Nav.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
 
 const Nav = ({ home, showFaves }) => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">EventSwipe</a>
+  <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: '#015249'} }>
+    <div className="navbar-brand" style={{ color: 'white' }}>EventSwipe</div>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item active">
-          <a className="nav-link" href="#" onClick={home}>
+          <a className="nav-link" href="#" onClick={home} style={{ color: 'white' }}>
             Search Events
             <span className="sr-only">(current)</span>
           </a>
         </li>
         <li className="nav-item active">
-          <a className="nav-link" href="#" onClick={showFaves}>
+          <a className="nav-link" href="#" onClick={showFaves} style={{ color: 'white' }}>
             Favorite Events
             <span className="sr-only">(current)</span>
           </a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" href="#">Signout</a>
-        </li>
       </ul>
     </div>
+    <div>
+      <div className="nav-item" style={{ color: 'white' }}>
+        <a className="nav-link disabled" href="#" style={{ color: 'rgb(165, 165, 175)' }}>Signout</a>
+      </div>
+    </div>
+    {/* </ul> */}
   </nav>
 );
 
