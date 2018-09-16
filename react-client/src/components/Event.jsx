b4530a76a291bf7d25d669e0cc8db5844d48e505
Alon Bibring <alon.bibring@gmail.com>
2018-09-16 18:46:05 -0400

    refactor of Event.jsx  variables

--- react-client/src/components/Event.jsx
+++ react-client/src/components/Event.jsx
@@ -50,7 +50,7 @@ class Event extends React.Component {
 
   render() {
     const { events } = this.props;
-    let event = events ? events[this.state.count] : events;
+    const event = events ? events[this.state.count] : events;
     const date = event && event.start ? new Date(moment(event.start.local)).toString().substring(0, 15) : event && event.local_date ? new Date(moment(event.local_date)).toString().substring(0, 15) : 'TBD';
     const time = event && event.local_time ? event.local_time : event && event.start ? event.start.local.substring(11) : 'Time Not Given';
     const description = event && event.plain_text_description ? event.plain_text_description : event && event.description && event.description.text ? event.description.text.replace(/<p[^>]*>/g, '').replace(/<\/p>/g, '').replace(/<a[^>]*>/g, '').replace(/<\/a>/g, '').replace(/<br[^>]*>/g, '').replace(/<\/br>/g, '').replace(/<img[^>]*>/g, '').replace(/<\/img>/g, '') : event && event.description ? event.description.replace(/<p[^>]*>/g, '').replace(/<\/p>/g, '').replace(/<a[^>]*>/g, '').replace(/<\/a>/g, '').replace(/<br[^>]*>/g, '').replace(/<\/br>/g, '').replace(/<img[^>]*>/g, '').replace(/<\/img>/g, '') : 'Click Link Below For Info';
@@ -58,11 +58,13 @@ class Event extends React.Component {
     const image = event && event.logo ? event.logo.url : event && event.featured_photo ? event.featured_photo.highres_link : event && event.group && event.group.photo ? event.group.photo.highres_link : 'http://tiny.cc/vaikyy'; 
     const price = event && event.is_free ? 'Free :)' : event && event.is_free === false ? 'Not Free' : event && event.fee ? event.fee.currency.replace('USD', '$') + event.fee.amount + ' ' + event.fee.description : 'N/A';
     const bold = { fontWeight: 'bold' };
+    const name = event && event.name && !event.name.text ? event.name : event && event.name && event.name.text ? event.name.text : event && event.group && event.group.name ? event.group.name : 'No Name Given';
+    const mapAddress = event && event.address ? event.address : event && event.venue ? event.venue.name : event && event.group ? event.group.name : 'No Name';
     return (
       <div>
         {event ? (
           <div className="row">
-            {console.log(event)}
+            {console.log('event object', event)}
             <div className="col">
               <div className="card" style={{ backgroundColor: 'rgba(47, 168, 184, 0.925)' }}>
                 <div className="card-body" style={{ width: 550, color: '#015249' }}>
@@ -72,7 +74,7 @@ class Event extends React.Component {
                     style={{ width: 500, height: 400 }} 
                     src={image} alt="Card image cap"
                   />
-                  <h5 className="card-title" style={{ marginTop: 10 }}>{event.name && !event.name.text ? event.name : event && event.name && event.name.text ? event.name.text : event && event.group && event.group.name ? event.group.name : 'No Name Given'}</h5>
+                  <h5 className="card-title" style={{ marginTop: 10 }}>{name}</h5>
                   <p className="card-text">
                     <span className="Loc"><b>Venue Address: </b> {address}</span>
                   </p>
@@ -89,8 +91,7 @@ class Event extends React.Component {
                     <span className="description"><b>Description:</b> {description}</span>
                   </p>
                   <p className="card-text">
-                    <span className="link"><b>Link:</b><a href={event.url || event.link} style={{ color: 'white' }}>&nbps; Check Out The Event</a></span>
-                    
+                    <span className="link"><b>Link:</b><a href={event.url || event.link} style={{ color: 'white' }}> Check Out The Event</a></span>
                   </p>
                   <button onClick={this.dislike}>
                     <span><img className="like-button" style={{ height: 50, width: 50}} src="https://pbs.twimg.com/profile_images/534074996562227200/OR7cp94I_400x400.png" alt="Like" /></span>
@@ -101,8 +102,8 @@ class Event extends React.Component {
                 </div>
               </div>
             </div>
-            <div className="col" style={{'textAlign': 'left'}}>
-              <iframe style={{ width: 400, height: 350 }} src={`https://www.google.com/maps/embed/v1/place?q=${event.address ? event.address : event.venue ? event.venue.name : event && event.group ? event.group.name : 'No Name'}&key=AIzaSyBMyF_JNu3kd5H4znq--2xe3WO-GRaC5NE`} allowFullScreen />
+            <div className="col" style={{ textAlign: 'left' }}>
+              <iframe style={{ width: 400, height: 350 }} src={`https://www.google.com/maps/embed/v1/place?q=${mapAddress}&key=AIzaSyBMyF_JNu3kd5H4znq--2xe3WO-GRaC5NE`} allowFullScreen />
             </div>
           </div>
         ) : (
