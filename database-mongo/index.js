022591edd050b119f505a7842405f9aa87dc1824
Paul Chung <pisc9211@gmail.com>
2018-09-17 15:25:11 -0400

    added comments to db

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://eventswipe:event1@ds017248.mlab.com:17248/events' || 'mongodb://localhost/test', { useMongoClient: true } );
+const {MLAB_USERNAME} = require('../config.js')
+const {MLAB_PASSWORD} = require('../config.js')
+
+//change username and password in config.js
+mongoose.connect(`mongodb://${MLAB_USERNAME}:${MLAB_PASSWORD}@ds017248.mlab.com:17248/events` || 'mongodb://localhost/test', { useMongoClient: true } );
 
 const db = mongoose.connection;
 
@@ -7,7 +11,7 @@ db.on('error', () => console.log('mongoose connection error'));
 
 db.once('open', () => console.log('mongoose connected successfully'));
 
-// event data for each user
+// event schema
 const eventSchema = mongoose.Schema({
   id: { type: String, unique: true },
   name: { type: String },
@@ -22,7 +26,7 @@ const eventSchema = mongoose.Schema({
   uid: { type: String }
 });
 
-// user data for login
+// user schema
 const userSchema = mongoose.Schema({
   uid: { type: String },
   userInfo: {
@@ -35,15 +39,14 @@ const userSchema = mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 const Event = mongoose.model('Event', eventSchema);
 
-// return events by username
-// add user here when authentication is setup
+// return events by uid
 const getAllEvents = (uid, cb) => {
   Event.find({ uid: uid }, (err, events) => {
     if (err) {
-      console.error(`err in selectEventByUsername db/index.js: ${err}`);
+      //console.error(`err in selectEventByUsername db/index.js: ${err}`);
       cb(err, null);
     } else {
-      console.log(`events in selectEventByUsername db/index.js: ${events}`);
+      //console.log(`events in selectEventByUsername db/index.js: ${events}`);
       cb(null, events);
     }
   });
@@ -51,6 +54,8 @@ const getAllEvents = (uid, cb) => {
 
 //adding a new user to database
 const addUser = (userData) => {
+  //create a new user document
+  //you can put this inside the User.find function below 
   const newUser = new User({
     uid: userData.uid,
     userInfo: {
@@ -58,12 +63,12 @@ const addUser = (userData) => {
       email: userData.email,
       photoUrl: userData.photoURL
     },
-    // favorites: { type: Object, required: true }
   });
   
+  //check if user exists in database before saving to db 
   User.find({uid: userData.uid}, (err, data) => {
     if (err) {
-      console.log(`err in User.find: ${err}`);
+      console.error(`err in User.find: ${err}`);
     } 
     if (data.length === 0) {
       newUser.save((err) => {
@@ -81,7 +86,8 @@ const addUser = (userData) => {
 
 // adding favorite events to database with username
 const addFavorite = (favorite, cb) => {
-  console.log('fave', favorite);
+  //fun fun fun...
+  //new event document
   var newEvent = new Event({
     id: favorite.id,
     name: favorite.name && !favorite.name.text ? favorite.name : favorite.name.text,
@@ -95,26 +101,27 @@ const addFavorite = (favorite, cb) => {
     logo: favorite.logo ? favorite.logo.url : favorite.featured_photo ? favorite.featured_photo.highres_link : favorite.group && favorite.group.photo ? favorite.group.photo.highres_link : null, 
     uid: favorite.uid, 
   });
-
+  //save new event to database
   newEvent.save(err => {
     if (err) {
-      console.error(`err in newEvent.save: ${err}`);
+      //console.error(`err in newEvent.save: ${err}`);
       cb(err);
     } else {
-      console.log('The new event has been saved into the database!');
+      //you can use the console.log below to check if data was saved into database
+      //console.log('The new event has been saved into the database!');
       cb();
     }
   });
 };
 
-// deleting favorite event
+// deleting favorite event by the id mongo automatically adds 
 const deleteFavorite = (mongoId, cb) => {
   Event.findOneAndRemove({ _id: mongoId }, err => {
     if (err) {
-      console.error(`err in deleteEvent: ${err}`);
+      //console.error(`err in deleteEvent: ${err}`);
       cb(err);
     } else {
-      console.log('Deleted from DB');
+      //console.log('Deleted from DB');
       cb();
     }
   });
